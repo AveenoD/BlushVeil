ebe4bbe352bd561c0d8b7d5e3ab263d5efacf3ac
Anees shaikh <aneesshaikh329@gmail.com>
2026-03-14 16:45:01 +0530

    Nav logo adjustment

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = ({ onSearch }) => {
                     src={logo}
                     alt="BlushVeil"
                     onClick={() => navigate('/')}
-                     className="h-10 w-10 cursor-pointer shrink-0 object-contain"
+                     className="h-30 cursor-pointer shrink-0 object-contain"
                 />
 
                 {/* Search Bar */}
