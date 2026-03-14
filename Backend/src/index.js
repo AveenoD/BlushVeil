ebe6ed3fb289e6d9ae3383499bf9670032af2d41
Anees shaikh <aneesshaikh329@gmail.com>
2026-03-14 11:42:30 +0530

    refactor: clean up index.js by removing unused imports and simplifying dotenv configuration

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,23 +1,16 @@
 import connectDB from "./database/index.js";
 import dotenv from "dotenv";
-import { app }from './app.js'
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
+import { app } from "./app.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-dotenv.config({ path: resolve(__dirname, './.env') });
-
-
+dotenv.config();
 
 connectDB()
-.then(()=> {
-    console.log('✅ MongoDB connected');
+.then(() => {
+    console.log("✅ MongoDB connected");
+
     const PORT = process.env.PORT || 8000;
 
     app.listen(PORT, () => {
-       console.log(`🚀 Server running on http://localhost:${PORT}`);
+        console.log(`🚀 Server running on ${PORT}`);
     });
-    
-
-})
+});
